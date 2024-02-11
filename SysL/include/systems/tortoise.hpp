31f037ad74ce20dac7f745609f3bc4eaef69faa4
Esilff <vbanari02@gmail.com>
2024-02-11 14:12:43 +0100

    [Dev] SysL::Tortoise - Building positions nodes (for tree) using the axiom

--- SysL/include/systems/tortoise.hpp
+++ SysL/include/systems/tortoise.hpp
@@ -2,14 +2,21 @@
 #define __SYSL_TORTOISE_HPP__
 #include <stack>
 #include <array>
+#include <vector>
 #include <string>
 #include <map>
 #include <glm/glm.hpp>
+
 
 #define RULE_EXPRESSION_DELIMITER '='
 
 
 namespace SysL {
+
+    struct SystemData {
+        std::vector<float> vertices;
+        std::vector<int> indices;
+    };
 
     class Tortoise {
     public:
@@ -18,6 +25,9 @@ namespace SysL {
     public:
 
         std::string generateAxiom(const std::string &expression, unsigned int iterations);
+        void computeAxiom(const std::string& axiom, glm::vec3 origin, float alpha);
+
+
 
 
         void addRule(const std::string& expression);
@@ -28,9 +38,14 @@ namespace SysL {
 
 
         std::map<const std::string, const std::string> rules;
+
+        std::vector<glm::vec3> positions;
         std::stack<glm::vec3> positionStack;
+        SystemData data;
 
         std::array<char, 9> operators {'+','-','&', '^', '<', '>', '|', '[', ']'};
+
+        void generateTrunk(glm::vec3 point, float radius, unsigned int segments);
 
 
 
--- SysL/src/systems/tortoise.cpp
+++ SysL/src/systems/tortoise.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "systems/tortoise.hpp"
+#include "glm/ext/matrix_transform.hpp"
 
 SysL::Tortoise::Tortoise() {}
 
@@ -33,4 +34,66 @@ std::string SysL::Tortoise::generateAxiom(const std::string &expression, unsigne
     }
     std::cout << "Processed result: " << result << std::endl;
     return result;
+}
+
+void SysL::Tortoise::computeAxiom(const std::string &axiom, glm::vec3 origin, float alpha) {
+    positions.push_back(origin);
+    glm::vec3 nextPosition = origin;
+    glm::vec4 nextVector = glm::vec4(0.0,0.0,1.0,0.0);
+    float radians = glm::radians(alpha);
+    for (auto character : axiom) {
+        switch(character){
+            case 'F':
+                nextPosition += glm::vec3(nextVector);
+                positions.push_back(nextPosition);
+                break;
+            case '+':
+                nextVector = glm::rotate(glm::mat4(1.0f), radians, glm::vec3(0.0f,0.0f,1.0f)) * nextVector;
+                break;
+            case '-':
+                nextVector = glm::rotate(glm::mat4(1.0f), -radians, glm::vec3(0.0f,0.0f,1.0f)) * nextVector;
+                break;
+            case '&':
+                nextVector = glm::rotate(glm::mat4(1.0f), -radians, glm::vec3(0.0f,1.0f,0.0f)) * nextVector;
+                break;
+            case '^':
+                nextVector = glm::rotate(glm::mat4(1.0f), radians, glm::vec3(0.0f,1.0f,0.0f)) * nextVector;
+                break;
+            case '<':
+                nextVector = glm::rotate(glm::mat4(1.0f), -radians, glm::vec3(1.0f,0.0f,0.0f)) * nextVector;
+                break;
+            case '>':
+                nextVector = glm::rotate(glm::mat4(1.0f), radians, glm::vec3(1.0f,0.0f,0.0f)) * nextVector;
+                break;
+            case '|':
+                break;
+            case '[':
+                positionStack.push(nextPosition);
+                break;
+            case ']':
+                nextPosition = positionStack.top();
+                positionStack.pop();
+                break;
+        }
+    }
+}
+
+void SysL::Tortoise::generateTrunk(glm::vec3 point, float radius, unsigned int segments) {
+    for (int i = 0; i < segments; i++) {
+        double angle = (2.0 * M_PI) / segments * i;
+        double dx = radius * cos( angle);
+        double dy = radius * sin(angle);
+        data.vertices.push_back(point.x + (float) dx);
+        data.vertices.push_back(point.y + (float) dy);
+        data.vertices.push_back(point.z);
+    }
+    if (data.vertices.size() <= segments) {
+        auto size = data.vertices.size();
+        for (auto i = size - 6; i < size; i++) {
+            data.indices.push_back((int)i + 1 > size ? (int) i : (int)i + 1);
+            data.indices.push_back((int)i);
+            data.indices.push_back((int)i - 6);
+            data.indices.push_back((int)i - 5);
+        }
+    }
 }
